b5dfd1a4be0b3005814594791386981832de9361
Tiago <tvhollowknight@gmail.com>
2025-03-06 01:07:52 -0300

    feat/test: added the possible_moves for the king class and written it's tests

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -4,15 +4,31 @@ class King < Piece
   # uses and returns [x, y] board notation
   #the board class uses [y, x] access
   def possible_moves(start, board)
+    iteration = 0
+    possible_moves = []
+    x_moves = [-1, -1, 0, 1, 1, 1, 0, -1]
+    y_moves = [0, 1, 1, 1, 0, -1, -1, -1]
+    while iteration < 8
+      x_position = x_moves[iteration] + start[0]
+      y_position = y_moves[iteration] + start[1]
+
+      if (x_position >= 0 && x_position <= 7) && (y_position >= 0 && y_position <= 7)
+        possible_moves << [x_position, y_position]
+      end
+      iteration += 1
+    end
+    possible_castle(start, board).each { |castle| possible_moves << castle }
+    possible_moves.delete_if { |address| address == [] }
   end
 
-  def possible_rocks(start, board)
-    return nil if board[start[1]][start[0]].was_moved
+  def possible_castle(start, board)
+    return [] if board[start[1]][start[0]].was_moved
 
-    possible_rocks = []
-    left_rock_possible = true
-    right_rock_possible = true
+    possible_castle = []
+    left_rock_possible = false
+    right_rock_possible = false
     unless board[start[1]][start[0] + 3] == 'empty' || board[start[1]][start[0] + 3].was_moved
+      right_rock_possible = true
       (1..2).each do |space|
         unless board[start[1]][start[0] + space] == 'empty'
           right_rock_possible = false
@@ -21,21 +37,15 @@ class King < Piece
     end
 
     unless board[start[1]][start[0] - 4] == 'empty' || board[start[1]][start[0] - 4].was_moved
+      left_rock_possible = true
       (1..3).each do |space|
         unless board[start[1]][start[0] - space] == 'empty'
           left_rock_possible = false
         end
       end
     end
-    possible_rocks << [start[1], start[0] + 2] if right_rock_possible
-    possible_rocks << [start[1], start[0] - 3] if left_rock_possible
+    possible_castle << [start[1], start[0] + 2] if right_rock_possible
+    possible_castle << [start[1], start[0] - 3] if left_rock_possible
+    possible_castle
   end
 end
-
-board = Array.new(8) { {} }
-board[0][4] = King.new('black', 'king')
-board[0][5] = 'empty'
-board[0][6] = 'empty'
-board[0][0] = 'empty'
-board[0][7] = Tower.new('black', 'tower')
-p board[0][4].possible_rocks([4, 0], board)
--- spec/pieces_spec.rb
+++ spec/pieces_spec.rb
@@ -25,7 +25,7 @@ describe Knight do
     it  'finds all possible 8 moves' do
       expect(knight_moves.possible_moves([3,3])).to match_array([[5, 2], [5, 4], [4, 1], [4, 5], [2, 1], [2, 5], [1, 2], [1, 4]])
     end
-    it  'does not show moves off the board' do
+    it  'does not show moves off the board_castle' do
       expect(knight_moves.possible_moves([0, 0])).to match_array([[1, 2], [2, 1]])
     end
   end
@@ -34,17 +34,92 @@ end
 describe Pawn do
   context 'finds the correct possible moves for the pawn' do
     let(:pawn_moves) {Pawn.new('pawn')}
-    empty_board = Array.new(8) { Array.new(8, 'empty')}
-    full_board = Array.new(8) { Array.new(8, '-')}
+    empty_board_castle = Array.new(8) { Array.new(8, 'empty')}
+    full_board_castle = Array.new(8) { Array.new(8, '-')}
 
-    it  'finds only forward moves with an empty board' do
-      expect(pawn_moves.possible_moves([1,1], empty_board)).to match_array([[1, 2], [1, 3]])
+    it  'finds only forward moves with an empty board_castle' do
+      expect(pawn_moves.possible_moves([1,1], empty_board_castle)).to match_array([[1, 2], [1, 3]])
     end
-    it  'does not show moves off the board with a full board' do
-      expect(pawn_moves.possible_moves([0, 0], full_board)).to match_array([[0, 2], [0, 1], [1, 1]])
+    it  'does not show moves off the board_castle with a full board_castle' do
+      expect(pawn_moves.possible_moves([0, 0], full_board_castle)).to match_array([[0, 2], [0, 1], [1, 1]])
     end
-    it  'finds all possible 4 moves with a full board' do
-      expect(pawn_moves.possible_moves([1,1], full_board)).to match_array([[0, 2], [1, 2], [2, 2], [1, 3]])
+    it  'finds all possible 4 moves with a full board_castle' do
+      expect(pawn_moves.possible_moves([1,1], full_board_castle)).to match_array([[0, 2], [1, 2], [2, 2], [1, 3]])
+    end
+  end
+end
+
+describe King do
+  context 'finds the possible castle moves' do
+    let(:king_castles) {King.new('king')}
+    board_castle = Array.new(8) { {} }
+    board_castle[0][4] = King.new('black', 'king')
+
+    it 'finds both moves when the spaces are empty and no piece has been moved' do
+      (1..6).each do |space|
+        board_castle[0][space] = 'empty' unless space == 4
+      end
+      board_castle[0][7] = Tower.new('black', 'tower')
+      board_castle[0][0] = Tower.new('black', 'tower')
+      expect(king_castles.possible_castle([4,0], board_castle)).to match_array([[0, 6], [0, 1]])
+    end
+    it 'finds no moves when the spaces are empty and the rooks have been moved' do
+      (1..6).each do |space|
+        board_castle[0][space] = 'empty' unless space == 4
+      end
+      board_castle[0][7] = Tower.new('black', 'tower')
+      board_castle[0][7].was_moved = true
+      board_castle[0][0] = Tower.new('black', 'tower')
+      board_castle[0][0].was_moved = true
+      expect(king_castles.possible_castle([4,0], board_castle)).to match_array([])
+    end
+    it 'finds no moves when the spaces are empty and the king has been moved' do
+      board_castle[0][4].was_moved = true
+      expect(king_castles.possible_castle([4,0], board_castle)).to match_array([])
+    end
+    it 'finds no moves when the spaces are full' do
+      (1..6).each do |space|
+        board_castle[0][space] = 'pawn' unless space == 4
+      end
+      board_castle[0][7] = Tower.new('black', 'tower')
+      board_castle[0][0] = Tower.new('black', 'tower')
+      expect(king_castles.possible_castle([4,0], board_castle)).to match_array([])
+    end
+    it 'can find only one move' do
+      (5..6).each do |space|
+        board_castle[0][space] = 'empty'
+      end
+      board_castle[0][4] = King.new('black', 'king')
+      board_castle[0][7] = Tower.new('black', 'tower')
+      board_castle[0][0] = 'empty'
+      expect(king_castles.possible_castle([4,0], board_castle).flatten).to match_array([0, 6])
+    end
+  end
+  context 'finds possible moves' do
+    let(:king_moves) {King.new('king')}
+    board_moves = Array.new(8) { {} }
+    board_moves[0][4] = King.new('black', 'king')
+
+    it 'does not find moves off the board' do
+      (0..7).each do |space|
+        board_moves[0][space] = 'empty' unless space == 4
+      end
+      expect(king_moves.possible_moves([4, 0], board_moves)).to match_array([[3, 0], [3, 1], [4, 1], [5, 1], [5, 0]])
+    end
+    it 'includes castle moves' do
+      (0..6).each do |space|
+        board_moves[0][space] = 'empty' unless space == 4
+      end
+      board_moves[0][7] = Tower.new('tower')
+      expect(king_moves.possible_moves([4, 0], board_moves)).to match_array([[3, 0], [3, 1], [4, 1], [5, 1], [5, 0], [0,6]])
+    end
+    it 'finds all the 8 possible moves' do
+      (0..7).each do |space|
+        board_moves[0][space] = 'empty'
+      end
+      board_moves[1][4] = King.new('king')
+      board_moves[1][4].was_moved = true
+      expect(king_moves.possible_moves([4, 1], board_moves)).to match_array([[3, 1], [3, 2], [4, 2], [5, 2], [5, 1], [5, 0], [4, 0], [3, 0]])
     end
   end
 end
